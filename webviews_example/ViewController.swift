347219662be04e2947747948feab5abb7b4a51ef
TGrooves-208 <tgrooves.208@gmail.com>
2016-05-03 21:56:49 -0600

    Initial Commit

---
+++ webviews_example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  webviews_example
+//
+//  Created by Gil Aguilar on 5/3/16.
+//  Copyright © 2016 redeye-dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
